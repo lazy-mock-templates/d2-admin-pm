62622fcbb1c321a5f0f8b5edf6a4ce5cdca020bc
wjk <977865769@qq.com>
2019-05-07 01:42:11 +0800

    update

---
+++ templates/config/config.js
@@ -0,0 +1,12 @@
+export default {
+    //server
+    RouteRelativePath:'/src/routes/',
+    ControllerRelativePath:'/src/controllers/',
+    ServiceRelativePath:'/src/services/',
+    ModelRelativePath:'/src/models/',
+    DBRelativePath:'/src/db/',
+
+    //page
+    APIRelativePath:'/src/api/',
+    PagesRelativePath:'/src/pages/'
+}
